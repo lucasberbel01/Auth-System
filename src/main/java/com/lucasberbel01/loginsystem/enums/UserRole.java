426ebfbc49c30a13b7bb8b8d7493e349409a2da3
lucasberbel01 <lucas@hberbel.com.br>
2026-09-08 15:47:09 -0300

    projeto spring boot iniciado, classes user e enum userRole criadas e conexao com banco de dados mysql

---
+++ src/main/java/com/lucasberbel01/loginsystem/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.lucasberbel01.loginsystem.enums;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/lucasberbel01/loginsystem/model/User.java
@@ -0,0 +1,38 @@
+package com.lucasberbel01.loginsystem.model;
+
+import com.lucasberbel01.loginsystem.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @Column(name = "userId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+}
