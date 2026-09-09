2859940ef22ce9d0c2ca953d34741afeabe8dd17
lucasberbel01 <lucas@hberbel.com.br>
2026-09-09 15:37:54 -0300

    prqueno ajuste na classe User

--- src/main/java/com/lucasberbel01/loginsystem/model/User.java
+++ src/main/java/com/lucasberbel01/loginsystem/model/User.java
@@ -2,13 +2,14 @@ package com.lucasberbel01.loginsystem.model;
 
 import com.lucasberbel01.loginsystem.enums.UserRole;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tb_user")
 public class User {
 
@@ -17,20 +18,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     @Column(nullable = false, unique = true)
     private String username;
 
-    @NotBlank
     @Column(nullable = false)
     private String password;
 
-    @NotBlank
-    @Email
     @Column(nullable = false, unique = true)
     private String email;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
