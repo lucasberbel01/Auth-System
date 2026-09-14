dc990f8a17ba4af20cbd5330c030ecb7e534a21d
lucasberbel01 <lucas@hberbel.com.br>
2026-09-14 14:33:05 -0300

    criado endpoint Post para criacao de conta

--- src/main/java/com/lucasberbel01/loginsystem/controller/UserController.java
+++ src/main/java/com/lucasberbel01/loginsystem/controller/UserController.java
@@ -3,14 +3,13 @@ package com.lucasberbel01.loginsystem.controller;
 import com.lucasberbel01.loginsystem.dto.UserRequestDTO;
 import com.lucasberbel01.loginsystem.dto.UserResponseDTO;
 import com.lucasberbel01.loginsystem.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -52,6 +51,17 @@ public class UserController {
 
     //================================================================================
     //POST
+    // ================================================================================
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO userResponseDTO = service.createUser(userRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
+    }
+
+    //================================================================================
+    //PUT E PATCH
     // ================================================================================
 
 }
