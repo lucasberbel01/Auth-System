cda97403bb65d8534a8da73ef89c81e62d89b525
lucasberbel01 <lucas@hberbel.com.br>
2026-09-16 14:16:37 -0300

    adicionado valores padroes para o endpoint getAll users pageable

--- src/main/java/com/lucasberbel01/loginsystem/controller/UserController.java
+++ src/main/java/com/lucasberbel01/loginsystem/controller/UserController.java
@@ -7,11 +7,12 @@ import com.lucasberbel01.loginsystem.dto.UserResponseDTO;
 import com.lucasberbel01.loginsystem.enums.UserRole;
 import com.lucasberbel01.loginsystem.service.UserService;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,7 +38,8 @@ public class UserController {
     // ================================================================================
 
     @GetMapping("/all")
-    public ResponseEntity<Page<UserResponseDTO>> getAllUsers(Pageable pageable) {
+    public ResponseEntity<Page<UserResponseDTO>> getAllUsers(@ParameterObject
+                                                                 @PageableDefault(size = 10, sort = "username") Pageable pageable) {
         return ResponseEntity.ok(service.getAllUsers(pageable));
     }
 
